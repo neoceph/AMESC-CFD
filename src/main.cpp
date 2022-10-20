ef1c66089bb641a175b376779a955e5a186d1549
Abdullah Al Amin <nirjhor18@gmail.com>
2022-10-20 09:03:29 -0500

    Change name and welcome message

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main (int arg, char *argv[])
 {
     int array[3] = {0, 1, 2};
-    cout << "Hello World!\n";
+    cout << "Welcome to AMESC-CFD!\n";
     std::cout << gGIT_VERSION << std::endl << gGIT_VERSION_SHORT << std::endl;
     
 }
