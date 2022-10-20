4583506bde9dbab5fa52e9044bfbfee64351b696
Abdullah Al Amin <nirjhor18@gmail.com>
2022-10-20 09:16:22 -0500

    Added versioning messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,38 @@
 #include<iostream>
+#include <string.h>
 
 #include "version.h"
 
-using namespace std;
 
 int main (int arg, char *argv[])
 {
-    int array[3] = {0, 1, 2};
-    cout << "Welcome to AMESC-CFD!\n";
-    std::cout << gGIT_VERSION << std::endl << gGIT_VERSION_SHORT << std::endl;
+    if (arg < 2)
+    {
+        int array[3] = {0, 1, 2};
+        std::cout << "Welcome to AMESC-CFD!\n";
+        std::cout << "Please provide appropriate arguments" << std::endl;
+        exit(1);
+        return 0;
+    }
+
+    else
+    {
+        if (strcmp(argv[1], "--version") == 0)
+        {
+            std::cout << "AMESC-CFD version: ";
+            std::cout << gGIT_VERSION << std::endl;
+            std::cout << "A thermal cfd solver for laser powder bed fusion additive manufacturing process modeling tool" << std::endl;
+            std::cout << "All rights reserved by Abdullah Al Amin and Satyajit Mojumder" << std::endl;
+        }
+
+        else if (strcmp(argv[1], "-v") == 0)
+        {
+            std::cout << gGIT_VERSION_SHORT << std::endl;
+        }
+        else
+        {
+            std::cout << "Argument not recognized!" << std::endl;
+        }
+    }
     
 }
